fba1384d3498e43ff065f43ef3457f232c2d8734
kalkidanteklay <kalkidanteklehaimanot@gmail.com>
2023-12-18 15:26:47 +0300

    added a file

---
+++ src/js/navbar.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('#button');
+const navbarMenu = document.querySelector('#mySidebar');
+ 
+    button.addEventListener('click', () => {
+        navbarMenu.classList.toggle('is-active');
+    
+        
+})
